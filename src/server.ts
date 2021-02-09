5f3a867b4dad819e6e04355e69de0808ad87667b
Heberth Junor <hjunor@gmail.com>
2021-02-09 10:46:59 -0300

    add: cors and aplication

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import express from 'express';
 import 'reflect-metadata';
 import { router } from './routes/routes';
 import './database';
+import cors from 'cors';
 const PORT = 3003;
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(router);
 
