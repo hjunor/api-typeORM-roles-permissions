89994efd93dc85d7d65e4f0560c098eb937e7b33
Heberth Junor <hjunor@gmail.com>
2021-02-10 07:54:09 -0300

    add: urlencoded

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
-import express from 'express';
+import express, { urlencoded } from 'express';
 
 import 'reflect-metadata';
-import { router } from './routes/routes';
 import './database';
+
+import { router } from './routes/routes';
 import cors from 'cors';
-const PORT = 3003;
+
+const PORT = 3001;
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(router);
-
+app.use(urlencoded({ extended: false }));
 app.listen(PORT, () => {
   console.log('server start 🔥', PORT);
 });
